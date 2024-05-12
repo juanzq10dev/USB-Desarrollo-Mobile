3f2c3279683f828d6fbe9de3bd43a3a3b9803848
Juan Zurita <juanzq10dev@gmail.com>
2024-05-12 15:04:50 -0400

    Add Lotery app functionality

--- ejercicios/03-LoteryApp/app/src/main/java/com/example/loteryapp/MainActivity.kt
+++ ejercicios/03-LoteryApp/app/src/main/java/com/example/loteryapp/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.loteryapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +19,14 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        var button = findViewById<Button>(R.id.next_button)
+        button.setOnClickListener{
+            var intent = Intent(this, SecondActivity::class.java)
+            var editText = findViewById<EditText>(R.id.editTextText)
+            intent.putExtra("user_name", editText.text.toString())
+            startActivity(intent)
         }
     }
 }
--- ejercicios/03-LoteryApp/app/src/main/java/com/example/loteryapp/SecondActivity.kt
+++ ejercicios/03-LoteryApp/app/src/main/java/com/example/loteryapp/SecondActivity.kt
@@ -1,12 +1,27 @@
 package com.example.loteryapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Telephony.Mms.Intents
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
 
 class SecondActivity : AppCompatActivity() {
+
+    private fun generateLottoNumbersText(totalNumbers: Int): String {
+        var res = ""
+        for (i in 0..totalNumbers) {
+            res += Random.nextInt(0, 100).toString() + " "
+        }
+        return res.trim()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +30,22 @@ class SecondActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        var button = findViewById<Button>(R.id.share_button)
+        var lottoNum = findViewById<TextView>(R.id.numbers_text_view)
+        lottoNum.text = generateLottoNumbersText(6)
+        var userName = intent.getStringExtra("user_name")
+
+        button.setOnClickListener{
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, "I am $userName my lotto number is: " + lottoNum.text.toString())
+                type = "text/plain"
+            }
+
+            val shareIntent = Intent.createChooser(sendIntent, null);
+            startActivity(shareIntent)
         }
     }
 }
